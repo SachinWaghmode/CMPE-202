907cb51cfd2a5bf399e621b6a79b67b3a17827ed
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-04-18 23:09:15 -0700

    Take input from command line

--- lastone/src/main/java/onemoretry/lastone/Umlparser.java
+++ lastone/src/main/java/onemoretry/lastone/Umlparser.java
@@ -4,12 +4,15 @@ public class Umlparser {
 
 		public static void main(String[] args)throws Exception{
 		
-		final String infile = "/Users/sachinwaghmode/eclipse/lastone/src/main/TestCase1/";
-		final String outfile = "/Users/sachinwaghmode/eclipse/lastone/src/main/TestCase1/";
+			
+		final String infile = args[0];
+		final String outfile = args[1];
 		
 		ParseClass pc = new ParseClass();
-	
+		//Check if class diagram
 		pc.compile(infile,outfile);
-	
+		//else if sequence diagram
+		//ParseSequence ps = new ParseSequence
+			// ps.compile(infile,outfile)
 	}
 }
